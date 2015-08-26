9b4a997a70e76fd26a6c8e5df6b551262f892cfc
AbdullahALT <abdulla20103@gmail.com>
2015-08-26 20:44:37 +0300

    *FEATURE: replace new line button by "Done" button *CHANGE: upgrade the app to support 23 apk

--- PhotosGalary/app/src/main/java/com/example/abo_tam/photosgalary/AddCource.java
+++ PhotosGalary/app/src/main/java/com/example/abo_tam/photosgalary/AddCource.java
@@ -3,9 +3,12 @@ package com.example.abo_tam.photosgalary;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class AddCource extends Activity {
@@ -21,6 +24,7 @@ public class AddCource extends Activity {
         setContentView(R.layout.activity_add_cource);
         Intent intent = getIntent();
 
+
         //TODO: set maximum 7 char
         editText = (EditText) findViewById(R.id.editText);
         button = (Button) findViewById(R.id.button);
@@ -31,14 +35,9 @@ public class AddCource extends Activity {
                     @Override
                     public void onClick(View v) {
                         boolean res;
-                        if (!editText.getText().toString().equals("")) {
+                        if (editText.getText().length() != 0) {
 
-                            res = db.insertCource(
-                                    new Cource(editText.getText().toString())
-                                    //TODO: you need to refresh mainActivity's listView here
-                            );
-                            MainActivity.setListView();
-
+                            res = addAndFinish();
                         } else
                             res = false;
                         Toast.makeText(AddCource.this, "the result is " + res, Toast.LENGTH_LONG).show();
@@ -46,6 +45,33 @@ public class AddCource extends Activity {
                     }
                 }
         );
+
+
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(isValid(actionId)) {
+                    addAndFinish();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+    }
+
+    private boolean addAndFinish(){
+        //TODO: enter key add two times
+        boolean res = db.insertCource(
+                new Cource(editText.getText().toString())
+        );
+        MainActivity.setListView();
+        finish();
+        return res;
+    }
+
+    private boolean isValid(int actionId){
+        return EditorInfo.IME_ACTION_DONE == actionId && editText.getText().toString().length() != 0 && !editText.getText().toString().contains(" ");
     }
 
 }
